38c9274d7479fdfab49afa7a127b19bdbabc5243
dhanyasreearun <dhanyas14@gmail.com>
2023-09-14 21:41:10 +0530

    Update in models

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,103 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/router";
+import { axios } from "axios";
+
+export default function SignupPage() {
+  const router = useRouter();
+  const [user, setUser] = React.useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const onSignup = async () => {};
+
+  return (
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img
+          className="mx-auto h-10 w-auto"
+          src="../../../assets/weighbridge.png"
+          alt="Amrita weighbridge"
+        />
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign up
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" action="#" method="POST">
+          <div>
+            <label className="block text-sm font-medium leading-6 text-gray-900">
+              Username
+            </label>
+            <div className="mt-2">
+              <input
+                id="username"
+                name="username"
+                type="text"
+                onChange={(e) => setUser({ ...user, username: e.target.value })}
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label className="block text-sm font-medium leading-6 text-gray-900">
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label className="block text-sm font-medium leading-6 text-gray-900">
+                Password
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
+                type="password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              onClick={onSignup}
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign up
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Already have an account?
+          <Link
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 pl-3"
+            href="/login"
+          >
+            Sign in here
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,23 @@
+import { error } from "console";
+import mongoose from "mongoose";
+
+export async function connect() {
+    try {
+
+        mongoose.connect(process.env.MONGO_URI!);
+        const connection = mongoose.connection;
+
+        connection.on('connected',()=>{
+            console.log('MongiDB connecected successfully');
+        });
+
+        connection.on('error',(err)=>{
+            console.log('MongiDB connection error. Please make sure mongoDB is running.'+err);
+            process.exit()
+        });
+
+    } catch(error) {
+        console.log('Something went wrong!');
+        console.log(error);
+    }
+}

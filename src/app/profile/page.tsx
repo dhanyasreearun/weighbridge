4977f60e5c6dade028272535cb494b3cb71d3646
dhanyasreearun <dhanyas14@gmail.com>
2023-09-14 21:41:10 +0530

    updates in profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,27 @@
-export default function ProfilePage() {
-  return <h1>Profile</h1>;
+import { Sidebar } from "../components/sidebar/sideBar";
+import { TopBar } from "../components/topbar/topbar";
+
+interface ProfileProps {
+  children: React.ReactNode;
+}
+
+const style = {
+  mainContainer:
+    "flex flex-col w-full h-screen pl-0 lg:space-y-4 lg:w-[calc(100%-16rem)]",
+  container: "bg-white h-screen overflow-hidden relative",
+  main: "bg-white h-screen overflow-auto pb-36 pt-4 px-2 md:pb-8 md:px-4 lg:px-6 lg:rounded-tl-3xl",
+};
+
+export default function ProfilePage(props: ProfileProps) {
+  return (
+    <div className={style.container}>
+      <div className="flex items-start">
+        <Sidebar mobileOrientation="end" />
+        <div className={style.mainContainer}>
+          <TopBar />
+          <main className={style.main}>{props.children}</main>
+        </div>
+      </div>
+    </div>
+  );
 }

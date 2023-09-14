b39ca8651acd63724be409ad2f8be1caed8dab57
dhanyasreearun <dhanyas14@gmail.com>
2023-09-14 21:41:11 +0530

    Update in vehicle master

--- src/app/components/content/content.tsx
+++ src/app/components/content/content.tsx
@@ -1,5 +1,9 @@
+import React, { ReactNode } from "react";
+
 interface ContentProps {
   title: string;
+  details: string;
+  children: ReactNode;
 }
 
 export function Content(props: ContentProps) {
@@ -38,99 +42,18 @@ export function Content(props: ContentProps) {
                 </span>
                 <div className="flex flex-col">
                   <span className="ml-2 font-bold text-black">
-                    {props.title ? props.title : " Google"}
+                    {props.title}
                   </span>
                   <span className="ml-2 text-sm text-gray-500">
-                    Google Inc.
+                    {props.details}
                   </span>
                 </div>
               </div>
-              <div className="flex items-center">
-                <button className="rounded-full border border-gray-200 p-1">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    className="h-4 w-4 text-yellow-500"
-                    fill="currentColor"
-                    viewBox="0 0 1792 1792"
-                  >
-                    <path d="M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5t-30.5 14.5q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z" />
-                  </svg>
-                </button>
-                <button className="text-gray-200">
-                  <svg
-                    width="25"
-                    height="25"
-                    fill="currentColor"
-                    viewBox="0 0 1792 1792"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M1088 1248v192q0 40-28 68t-68 28h-192q-40 0-68-28t-28-68v-192q0-40 28-68t68-28h192q40 0 68 28t28 68zm0-512v192q0 40-28 68t-68 28h-192q-40 0-68-28t-28-68v-192q0-40 28-68t68-28h192q40 0 68 28t28 68zm0-512v192q0 40-28 68t-68 28h-192q-40 0-68-28t-28-68v-192q0-40 28-68t68-28h192q40 0 68 28t28 68z" />
-                  </svg>
-                </button>
-              </div>
             </div>
-            <div className="mb-4 flex items-center justify-between space-x-12">
-              <span className="flex items-center rounded-md bg-gray-200 px-2 py-1 text-xs font-semibold text-gray-500">
-                PROGRESS
-              </span>
-              <span className="flex items-center rounded-md border border-red-400 bg-white px-2 py-1 text-xs font-semibold  text-red-400">
-                HIGH PRIORITY
-              </span>
+            <div className="flex min-h-full flex-col justify-center px-6 pb-5 lg:px-8">
+              {props.children}
             </div>
-            <div className="m-auto block">
-              <div>
-                <span className="inline-block text-sm text-gray-500">
-                  Task done :<span className="font-bold text-gray-700">25</span>
-                  /50
-                </span>
-              </div>
-              <div className="mt-2 h-2 w-full rounded-full bg-gray-200">
-                <div className="h-full w-1/2 rounded-full bg-purple-500 text-center text-xs text-white" />
-              </div>
-            </div>
-            <div className="my-4 flex items-center justify-start space-x-4">
-              <span className="flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-semibold text-green-500">
-                IOS APP
-              </span>
-              <span className="flex items-center rounded-md bg-blue-100 px-2 py-1 text-xs font-semibold text-blue-500">
-                UI/UX
-              </span>
-            </div>
-            <div className="flex -space-x-2">
-              <a href="#">
-                <img
-                  className="inline-block h-10 w-10 rounded-full object-cover ring-2 ring-white"
-                  src="/images/5.jpg"
-                  alt="Guy"
-                />
-              </a>
-              <a href="#" className="">
-                <img
-                  className="inline-block h-10 w-10 rounded-full object-cover ring-2 ring-white"
-                  src="/images/6.jpg"
-                  alt="Max"
-                />
-              </a>
-              <a href="#" className="">
-                <img
-                  className="inline-block h-10 w-10 rounded-full object-cover ring-2 ring-white"
-                  src="/images/7.jpg"
-                  alt="Charles"
-                />
-              </a>
-              <a href="#" className="">
-                <img
-                  className="inline-block h-10 w-10 rounded-full object-cover ring-2 ring-white"
-                  src="/images/8.jpg"
-                  alt="Jade"
-                />
-              </a>
-            </div>
-            <span className="mt-4 flex w-36 items-center rounded-md bg-yellow-100 px-2 py-1 text-xs font-semibold text-yellow-500">
-              DUE DATE : 18 JUN
-            </span>
+            <div className="flex -space-x-2"></div>
           </div>
         </div>
       </div>
--- src/app/vehiclemaster/page.tsx
+++ src/app/vehiclemaster/page.tsx
@@ -1,15 +1,151 @@
 "use client";
+import React, { useEffect } from "react";
 import { Content } from "../components/content/content";
 import ProfileLayout from "../profile/page";
+import { toast } from "react-hot-toast";
 
 interface ChildrenProps {
   children: React.ReactNode;
 }
 
 export default function ProjectsPage() {
+  const [vehiclemaster, setVehicleMaster] = React.useState({
+    vehiclenumber: "",
+    vehicleowner: "",
+    vehicleweight: "",
+    vehicletype: "",
+  });
+
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+
+  const [loading, setLoading] = React.useState(false);
+
+  const saveVehicleMaster = async () => {
+    try {
+      setLoading(true);
+    } catch (error: any) {
+      console.log("Sign up failed", error.message);
+
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (
+      vehiclemaster.vehiclenumber.length > 0 &&
+      vehiclemaster.vehicleowner.length > 0 &&
+      vehiclemaster.vehicleweight.length > 0 &&
+      vehiclemaster.vehicletype.length > 0
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [vehiclemaster]);
+
   return (
     <ProfileLayout>
-      <Content title="Arun"></Content>
+      <Content title="Vehicle Master" details="Fill details about the vehicle">
+        <div>
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6" action="#" method="POST">
+              <div>
+                <label className="block text-sm font-medium leading-6 text-gray-900">
+                  Vehicle Number
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="vehiclenumber"
+                    name="vehiclenumber"
+                    type="text"
+                    onChange={(e) =>
+                      setVehicleMaster({
+                        ...vehiclemaster,
+                        vehiclenumber: e.target.value,
+                      })
+                    }
+                    required
+                    className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div>
+                <label className="block text-sm font-medium leading-6 text-gray-900">
+                  Owner Name
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="owner"
+                    name="owner"
+                    type="text"
+                    onChange={(e) =>
+                      setVehicleMaster({
+                        ...vehiclemaster,
+                        vehicleowner: e.target.value,
+                      })
+                    }
+                    required
+                    className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div>
+                <label className="block text-sm font-medium leading-6 text-gray-900">
+                  Total Weight
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="vehicleweight"
+                    name="vehicleweight"
+                    type="text"
+                    onChange={(e) =>
+                      setVehicleMaster({
+                        ...vehiclemaster,
+                        vehicleweight: e.target.value,
+                      })
+                    }
+                    required
+                    className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div>
+                <label className="block text-sm font-medium leading-6 text-gray-900">
+                  Vehicle Type
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="vehicletype"
+                    name="vehicletype"
+                    type="text"
+                    onChange={(e) =>
+                      setVehicleMaster({
+                        ...vehiclemaster,
+                        vehicletype: e.target.value,
+                      })
+                    }
+                    required
+                    className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div>
+                <button
+                  onClick={saveVehicleMaster}
+                  className={`flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${
+                    buttonDisabled ? "disabled:opacity-50" : ""
+                  }`}
+                  disabled={buttonDisabled}
+                >
+                  Save
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </Content>
     </ProfileLayout>
   );
 }
